4c5c2ef251a3e17b0d05ff9ca2e8d3289548a69e
MatthewL09 <matthew.r.luna09@gmail.com>
2022-04-06 09:46:21 -0500

    delete

--- eny.py
+++
@@ -1,8 +0,0 @@
-host = '157.230.209.171'
-username = 'innis_1666'
-user = username
-password = 'wy1hPZc9W4uN3eLRfdlU7YLT5wrBz8eJ'
-
-def get_db_url(database, user=user, password=password, host=host):
-    url = f'mysql+pymysql://{user}:{password}@{host}/{database}'
-    return url
